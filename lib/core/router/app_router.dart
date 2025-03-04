60ec92a44c042b0c02ea14031774c7cd17300b0e
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-04 17:50:39 +0200

    provide sing in cubit
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hr/features/authentication/presentation/cubit/sign_in_cubit.dart';
 import 'package:hr/features/authentication/presentation/sign_in_screen.dart';
 
 abstract class AppRouter {
   static Map<String, Widget Function(BuildContext)> routes = {
-    '/': (context) => SignInScreen(),
+    '/':
+        (context) => BlocProvider(
+          create: (context) => SignInCubit(),
+          child: SignInScreen(),
+        ),
   };
 }
