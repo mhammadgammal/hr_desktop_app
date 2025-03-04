3c161a1cfad8e1d52acadfb657be9a787e1e579c
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-04 17:54:17 +0200

    add new translation extension on string
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

--- lib/core/extensions/extensions.dart
+++ lib/core/extensions/extensions.dart
@@ -1,5 +1,7 @@
-import 'dart:ui';
 import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:hr/core/utils/localization/app_localization.dart';
 
 extension ColorHex on Color {
   static Color fromHex(String hex) {
@@ -13,5 +15,15 @@ extension ColorHex on Color {
 extension StringLog on String {
   void logg() {
     log(this);
+  }
+}
+
+extension Transalation on String {
+  String tr(BuildContext context) {
+    try {
+      return AppLocalizations.of(context).translate(this);
+    } catch (_) {
+      return this;
+    }
   }
 }
