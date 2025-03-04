3be4159faa0ed3e2d4f28c49ee1954934b1e2c99
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-04 17:55:15 +0200

    implement logo and brand name section
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

---
+++ lib/features/authentication/presentation/widgets/logo_section.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hr/core/assets/app_images.dart';
+import 'package:hr/core/theme/app_colors.dart';
+import 'package:hr/core/theme/app_theme.dart';
+
+class LogoSection extends StatelessWidget {
+  const LogoSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SvgPicture.asset(AppImages.logo),
+        SizedBox(height: 10.h),
+        RichText(
+          text: TextSpan(
+            text: 'HR',
+            style: GoogleFonts.kaushanScript(
+              color: AppColors.secondaryColor,
+              fontSize: 48.sp,
+              fontWeight: FontWeight.w500,
+            ),
+            children: [
+              TextSpan(
+                text: ' infinity',
+                style: GoogleFonts.kantumruyPro(
+                  color:
+                      AppTheme.isDarkMode(context)
+                          ? AppColors.white
+                          : AppColors.black,
+                  fontSize: 48.sp,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
