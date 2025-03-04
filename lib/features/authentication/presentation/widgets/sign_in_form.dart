c23ae0b6494cedaaf3e72bdae49832652a4bf5d8
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-04 17:55:29 +0200

    finish design
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

---
+++ lib/features/authentication/presentation/widgets/sign_in_form.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hr/core/assets/app_images.dart';
+import 'package:hr/core/widgets/buttons/custom_filled_button.dart';
+import 'package:hr/core/widgets/text_form_field/email_text_form_field.dart';
+import 'package:hr/core/widgets/text_form_field/password_text_form_field.dart';
+import 'package:hr/features/authentication/presentation/cubit/sign_in_cubit.dart';
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SignInCubit, SignInState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        var cubit = SignInCubit.get(context);
+        return SizedBox(
+          width: 550.w,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Form(
+              key: GlobalKey<FormState>(),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset(AppImages.loginBackground),
+                  EmailTextFormField(
+                    controller: cubit.emailController,
+                    label: 'Email',
+                  ),
+                  SizedBox(height: 10.0.h),
+                  PasswordTextFormField(
+                    controller: cubit.passwordController,
+                    label: 'Password',
+                  ),
+                  SizedBox(height: 20.0.h),
+                  CustomFilledButton(
+                    width: double.infinity,
+                    onPressed: cubit.signIn,
+                    title: 'Sign In',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
