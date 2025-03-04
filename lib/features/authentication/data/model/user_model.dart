073fd1948131a2e63a2b2719a014676d38b4ea8f
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-04 03:19:07 +0200

    implement a user model to manage data base scheme
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

---
+++ lib/features/authentication/data/model/user_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart' show immutable;
+
+@immutable
+class UserModel {
+  final int id;
+  final String name;
+  final String email;
+  final String password;
+  final String profilePicturePath;
+
+  const UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.profilePicturePath,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'name': name,
+    'email': email,
+    'password': password,
+    'profile_picture_path': profilePicturePath,
+  };
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    id: json['id'],
+    name: json['name'],
+    email: json['email'],
+    password: json['password'],
+    profilePicturePath: json['profile_picture_path'],
+  );
+}
