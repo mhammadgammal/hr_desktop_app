1b6af47ffe4cfbfe23ff1044961c9087fa45b8a4
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-04 04:06:18 +0200

    add helper class app_images and include app logo as SVG asset
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

---
+++ lib/core/assets/app_images.dart
@@ -0,0 +1,6 @@
+abstract class AppImages {
+  static final String logo = 'assets/images/svgs/logo.svg';
+  static final String loginBackground = 'assets/images/svgs/auth.svg';
+  static final String profile = 'assets/images/svgs/profile.svg';
+  static final String wellDone = 'assets/images/svgs/well_done.svg';
+}
