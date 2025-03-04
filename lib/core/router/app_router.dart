11aaf2e0a8514492712b94688e502f97090660fc
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-04 04:05:44 +0200

    return sign in screen as root instead of test screen
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hr/features/authentication/presentation/sign_in_screen.dart';
 
 abstract class AppRouter {
   static Map<String, Widget Function(BuildContext)> routes = {
-    '/': (context) => Center(child: Text('Home Screen')),
+    '/': (context) => SignInScreen(),
   };
 }
