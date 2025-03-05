9c98017db16a98db54a47e8a34a8f71b22355e46
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-05 19:57:56 +0200

    create a new helper for dialogs
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

---
+++ lib/core/widgets/dialog_helper/dialog_helper.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class DialogHelper {
+  static void showFailureDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: const Text('Error'),
+            content: Text(message),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+    );
+  }
+}
