5dd0a92ad4c0a143c038ed44dab42713b27a0c2d
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-06 01:56:45 +0200

    - add path to icons folder - implement helper class accessing icons assets
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

---
+++ lib/core/assets/app_icons.dart
@@ -0,0 +1,7 @@
+abstract class AppIcons {
+  static String homeIc = 'assets/icons/home.svg';
+  static String bellIc = 'assets/icons/bell.svg';
+  static String addDocumentIc = 'assets/icons/add_document.svg';
+  static String settingsIc = 'assets/icons/tooth.svg';
+  static String logoutIc = 'assets/icons/logout.svg';
+}
