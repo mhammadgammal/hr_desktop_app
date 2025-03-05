264c024e6bfa78c7517f42ca75bff522ac1113be
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-05 02:12:51 +0200

    add new login background for dark theme
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

--- lib/core/assets/app_images.dart
+++ lib/core/assets/app_images.dart
@@ -1,6 +1,7 @@
 abstract class AppImages {
   static final String logo = 'assets/images/svgs/logo.svg';
   static final String loginBackground = 'assets/images/svgs/auth.svg';
+  static final String loginBackgroundDark = 'assets/images/svgs/auth_dark.svg';
   static final String profile = 'assets/images/svgs/profile.svg';
   static final String wellDone = 'assets/images/svgs/well_done.svg';
 }
