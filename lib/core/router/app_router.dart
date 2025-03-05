fe8ef415dd83cb19f794f58672afdb33e0d66015
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-05 19:56:55 +0200

    add new route "main_screen"
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hr/core/router/router_keys.dart';
 import 'package:hr/features/authentication/presentation/cubit/sign_in_cubit.dart';
 import 'package:hr/features/authentication/presentation/sign_in_screen.dart';
 
 abstract class AppRouter {
   static Map<String, Widget Function(BuildContext)> routes = {
-    '/':
+    RouterKeys.home:
         (context) => BlocProvider(
-          create: (context) => SignInCubit(),
+          create: (context) =>
+          SignInCubit()
+            ..createUser(),
           child: SignInScreen(),
         ),
+    RouterKeys.mainScreen: (_) => Center(child: Text('Main Screen'),),
   };
 }
--- lib/core/router/router_keys.dart
+++ lib/core/router/router_keys.dart
@@ -1,3 +1,4 @@
 abstract class RouterKeys {
   static String home = '/';
+  static String mainScreen = '/main_screen';
 }
