0a5297c3ee257913b6e5f30fe03514ee0f618dbe
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-05 02:12:27 +0200

    add a package to resize windows window size
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hr/core/di/di.dart';
 import 'package:hr/infinity_hr_app.dart';
+import 'package:window_size/window_size.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await init();
+
+  if (Platform.isWindows) {
+    setWindowMinSize(const Size(1200, 800)); // Minimum width and height
+  }
   runApp(const InfinityHrApp());
 }
