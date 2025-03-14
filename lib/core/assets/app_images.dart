83222b67bcd3ce0ec9bab38481c285a1b181f10d
Mohamed Gammal <mhammadgamal17@gmail.com>
2025-03-15 00:12:18 +0200

    add new placeholder
    Signed-off-by: Mohamed Gammal <mhammadgamal17@gmail.com>

--- lib/core/assets/app_images.dart
+++ lib/core/assets/app_images.dart
@@ -4,4 +4,6 @@ abstract class AppImages {
   static final String loginBackgroundDark = 'assets/images/svgs/auth_dark.svg';
   static final String profile = 'assets/images/svgs/profile.svg';
   static final String wellDone = 'assets/images/svgs/well_done.svg';
+  static final String profilePlaceholder =
+      'assets/images/svgs/profile_placeholder.svg';
 }
